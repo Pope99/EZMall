4a797807aa135537d970fa94f42c2494369f5efa
Pope <a5352532@hotmail.com>
2025-02-21 14:23:40 +0800

    backend setting with docker startup

---
+++ backend/src/main/java/com/myProj/ezmall/EzMallApplication.java
@@ -0,0 +1,13 @@
+package com.myProj.ezmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzMallApplication.class, args);
+	}
+
+}
